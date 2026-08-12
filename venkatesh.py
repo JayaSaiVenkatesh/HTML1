669aeac585f9e4fca4e504bb1ed09a586a7a55e9
JayaSaiVenkatesh <jayasai00618@gmail.com>
2026-08-12 10:34:10 +0530

    Add variable initialization and type printing
    Initialize variables and print their values and types.

---
+++ venkatesh.py
@@ -0,0 +1,10 @@
+a=4
+b=6
+c=8
+d=2
+e=3
+print(f"value={a} and Data type={type(a)}")
+print(f"value={b} and data type={type(b)}")
+print(f"value={c} and data type={type(c)}")
+print(f"value={d} and data type={type(d)}")
+print(f"value={e} and data type={tyoe(e)}")
